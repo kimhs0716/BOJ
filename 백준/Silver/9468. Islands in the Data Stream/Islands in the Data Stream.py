3016ebd70faac9db04cc63f409a4901b40c993f5
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-08-07 07:36:30 +0900

    [Silver V] Title: Islands in the Data Stream, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9468. Islands in the Data Stream/Islands in the Data Stream.py
@@ -0,0 +1,13 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+for t in range(n):
+    arr = list(map(int, input().split()))[1:]
+    cnt = 0
+    for i in range(1, 14):
+        for j in range(i + 1, 15):
+            if min(arr[i : j]) > max(arr[i - 1], arr[j]):
+                cnt += 1
+    print(t + 1, cnt)
