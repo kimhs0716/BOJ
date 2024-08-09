5f1213290d33d298d6dd1774b4784bc399631d50
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-08-09 22:22:11 +0900

    [Gold IV] Title: Skill, Time: 700 ms, Memory: 128676 KB -BaekjoonHub

--- 백준/Gold/9356. Skill/Skill.py
+++ 백준/Gold/9356. Skill/Skill.py
@@ -2,13 +2,15 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 mod = int(1e9) + 7
+dp = [[0] * 10, [0] * 10]
 
 tc = int(input())
 for _ in range(tc):
     n = int(input())
-    dp = [[0] * 10 for _ in range(n + 1)]
     for i in range(10): dp[1][i] = 1
     for i in range(2, n + 1):
+        if i >= len(dp):
+            dp.append([0] * 10)
         s = 0
         for j in range(10):
             s = (s + dp[i - 1][j]) % mod
