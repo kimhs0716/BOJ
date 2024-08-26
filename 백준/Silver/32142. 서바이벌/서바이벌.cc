7736cc40375f9942f1127d062e042f5cd6e973e6
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-08-27 07:25:54 +0900

    [Silver III] Title: 서바이벌, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32142. 서바이벌/서바이벌.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+#define endl '\n'
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main() {
+	FASTIO;
+    
+    cout << "-1\n";
+
+	return 0;
+}
+
