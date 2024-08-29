098fc14f69fb9efee158310f3b8bbe56a00c8e43
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-08-29 18:09:32 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 216 ms, Memory: 129628 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,30 +1,89 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
 
-try:
-    sys.stdin = open('boj.in', 'r', encoding='utf-8')
-except FileNotFoundError:
-    pass
+input = sys.stdin.readline
 
 
-size = 10 ** 6
-isprime = [True] * (size + 1)
-isprime[0] = False
-isprime[1] = False
 
-i = 2
-while i <= size:
-    j = 2
-    while i * j <= size:
-        isprime[i * j] = False
-        j += 1
-    i += 1
-    while i <= size and isprime[i] == 0: i += 1
+def Era(size):
 
-for _ in range(int(input())):
-    n = int(input())
-    cnt = 0
-    for i in range(2, n // 2 + 1):
-        if not isprime[i]: continue
-        if isprime[n - i]: cnt += 1
-    print(cnt)
+    arr = [1 for _  in range(size+1)]
+
+    
+
+    arr[0] = 0
+
+    arr[1] = 0
+
+    
+
+    inx = 2
+
+    multiplier = 2
+
+    
+
+    while inx<=size:
+
+        multiplier = 2
+
+        if arr[inx] == 1:
+
+            while inx * multiplier <= size:
+
+                arr[inx * multiplier] = 0
+
+                multiplier = multiplier + 1
+
+        inx = inx + 1
+
+    return arr
+
+                
+
+num = []
+
+
+
+for _ in range(int(input().rstrip())):
+
+    num.append(int(input().rstrip()))
+
+    
+
+MAX = max(num)
+
+
+
+prime = Era(MAX)
+
+dic = {}
+
+
+
+for inx, x in enumerate(prime):
+
+    if x == 1:
+
+        dic[inx]=True
+
+
+
+for k in num:
+
+    count = 0
+
+    for t in dic:
+
+        if t>(k//2):
+
+            break
+
+        else:
+
+            if (k-t) in dic:
+
+                #print(f"k,t = :{k,t,k-t}") 디버깅용코드
+
+                count = count + 1
+
+    print(count)
