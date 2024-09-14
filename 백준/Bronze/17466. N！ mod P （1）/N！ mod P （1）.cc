1a84915bfd4eb575dbc519a4b21d0cf272ae4e6b
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-09-14 12:24:29 +0900

    [Bronze II] Title: N! mod P (1), Time: 1144 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -17,13 +17,36 @@ using pii = pair<int, int>;
 
 constexpr int INF = 2e8;
 
+ll fac(ll n, ll p) {
+    ll ans = 1;
+    for (ll i = 1; i <= n; i++) ans = ans * i % p;
+    return ans;
+}
+
+ll _pow(ll n, ll r, ll m) {
+    if (r == 0) return 1;
+    ll ret = _pow(n, r >> 1, m);
+    ret = ret * ret % m;
+    if (r & 1) ret = ret * n % m;
+    return ret;
+}
+
+ll inv(ll n, ll p) {
+    return _pow(n, p - 2, p);
+}
+
 int main() {
     FASTIO;
     _init();
 
     ll n, p; cin >> n >> p;
-    ll ans = 1;
-    for (ll i = 1; i <= n; i++) ans = ans * i % p;
+    ll d = p - n;
+    ll s = (d % 2) ? -1 : 1;
+    ll fd = fac(d - 1, p);
+    ll invf = inv(fd, p);
+    ll ans = s * invf % p;
+    if (ans < 0) ans += p;
+
     cout << ans << endl;
 
     return 0;
