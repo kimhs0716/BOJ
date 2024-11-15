64a56896c4f830e6623a1e62ca9fa80a0ace6728
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-11-15 13:59:49 +0900

    [Bronze II] Title: 게임말 올려놓기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31995. 게임말 올려놓기/게임말 올려놓기.py
@@ -0,0 +1,16 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+try:
+    sys.stdin = open('boj.in', 'r', encoding='utf-8')
+    # sys.stdout = open('boj.out', 'w', encoding='utf-8')
+    pass
+except FileNotFoundError:
+    pass
+
+
+n = int(input())
+m = int(input())
+
+print(2 * (n - 1) * (m - 1))
