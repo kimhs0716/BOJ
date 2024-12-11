32cf110b305bce028b111bc314d807b47f7ef6c4
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2024-12-11 12:29:26 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,15 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+inf = float('inf')
+
+try:
+    sys.stdin = open('boj.in', 'r', encoding='utf-8')
+    # sys.stdout = open('boj.out', 'w', encoding='utf-8')
+except FileNotFoundError:
+    pass
+
+
+n = int(input())
+arr = '0;12 1600;11 894;11 1327;10 1311;9 1004;9 1178;9 1357;8 837;7 1055;6 556;6 773'.split(';')
+print(arr[n])
