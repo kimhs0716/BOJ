b039be5ce9ed17e6548fd94d02cb484c968a0d4d
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-11-20 00:40:01 +0900

    [Gold I] Title: Beast Bullies, Time: 88 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Gold/27384. Beast Bullies/Beast Bullies.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using ull = unsigned long long;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> &arr) {
+    os<<'(';
+    for (auto &x: arr) os<<x<<' ';
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+constexpr ll INF = 2e18;
+
+void preprocess() {
+    ll i, j;
+
+}
+
+void solve(ll testcase) {
+    ll i, j, k;
+    ll n; cin>>n;
+    vl arr(n); cin>>arr;
+    sort(arr.rbegin(), arr.rend());
+    if (accumulate(arr.begin()+1, arr.end(), 0LL) < arr[0]) {
+        cout<<"1\n";
+        return;
+    }
+    ll c=0, t=0, sum=arr[0];
+    ll ans=1;
+    for (i=1;i<n;i++) {
+        t++;
+        c += arr[i];
+        if (c>=sum) {
+            ans += t;
+            sum += c;
+            c=0, t=0;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
