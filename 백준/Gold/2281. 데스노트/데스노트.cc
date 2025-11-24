108491027b424fcdd6bac9623c9d8a5c08a282b4
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-11-24 23:34:45 +0900

    [Gold II] Title: 데스노트, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/2281. 데스노트/데스노트.cc
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    ll n, m; cin>>n>>m;
+    vl arr(n); cin>>arr;
+    vl dp(m, INF);
+    dp[arr[0]-1] = 0;
+    for (i=1;i<n;i++) {
+        vl ndp(m, INF);
+        for (j=0;j<m;j++) {
+            if (j-arr[i]-1>=0 && dp[j-arr[i]-1]!=INF) ndp[j] = dp[j-arr[i]-1];
+            ndp[arr[i]-1] = min(ndp[arr[i]-1], dp[j] + (m-j-1) * (m-j-1));
+        }
+        swap(dp, ndp);
+        // for (auto x: dp) {
+        //     if (x==INF) cout<<"I ";
+        //     else cout<<x<<' ';
+        // }
+        // cout<<endl;
+    }
+    cout<<*min_element(dp.begin(), dp.end())<<endl;
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
+
