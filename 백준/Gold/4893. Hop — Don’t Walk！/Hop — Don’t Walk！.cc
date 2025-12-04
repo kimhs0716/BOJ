e362ff1da10abc7e3dffc1aa9e01622b9433c498
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-12-04 23:53:20 +0900

    [Gold III] Title: Hop — Don’t Walk!, Time: 148 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/4893. Hop — Don’t Walk！/Hop — Don’t Walk！.cc
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+// struct SEG {
+//     ll n;
+//     vl seg;
+//     SEG(ll n) : n(n), seg(n<<2) {}
+//     void update(ll i, ll l, ll r, ll tar, ll val) {
+//         if (l==r) {
+//             seg[i] = val;
+//             return;
+//         }
+//         ll m=l+r>>1;
+//         if (tar<=m) update(i<<1, l, m, tar, val);
+//         else update(i<<1|1, m+1, r, tar, val);
+//         seg[i] = seg[i<<1] + seg[i<<1|1];
+//     }
+// };
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    tc = 0;
+    while (true) {
+        string s; cin>>s;
+        if (s=="---") break;
+        ll n=s.size();
+        vl arr(n);
+        ll f = -1;
+        for (i=0;i<n;i++) {
+            if (s[i] == 'W') arr[i] = 0;
+            else if (s[i] == 'B') arr[i] = 1;
+            else arr[i] = 2, f = i;
+        }
+        auto check = [](vl &t) {
+            ll i=0, n=t.size();
+            for (;i<n;i++) {
+                if (t[i]==1) break;
+            }
+            if (i==n) return true;
+            for (;i<n;i++) {
+                if (t[i]==0) break;
+            }
+            for (;i<n;i++) {
+                if (t[i]==1) return false;
+            }
+            return true;
+        };
+        function<ll(vl&, ll, ll, ll&)> dfs = [n, &check, &dfs](vl &t, ll f, ll d, ll &md) {
+            if (check(t)) return md = d;
+            if (d==10) return INF;
+            ll ret = INF;
+            if (f-2>=0) {
+                t[f-2] = !t[f-2];
+                swap(t[f], t[f-2]);
+                if (d+1<md) ret = min(ret, dfs(t, f-2, d+1, md));
+                md = min(ret, md);
+                swap(t[f], t[f-2]);
+                t[f-2] = !t[f-2];
+            }
+            if (f-1>=0) {
+                swap(t[f], t[f-1]);
+                if (d+1<md) ret = min(ret, dfs(t, f-1, d+1, md));
+                md = min(ret, md);
+                swap(t[f], t[f-1]);
+            }
+            if (f+2<n) {
+                t[f+2] = !t[f+2];
+                swap(t[f], t[f+2]);
+                if (d+1<md) ret = min(ret, dfs(t, f+2, d+1, md));
+                md = min(ret, md);
+                swap(t[f], t[f+2]);
+                t[f+2] = !t[f+2];
+            }
+            if (f+1<n) {
+                swap(t[f], t[f+1]);
+                if (d+1<md) ret = min(ret, dfs(t, f+1, d+1, md));
+                md = min(ret, md);
+                swap(t[f], t[f+1]);
+            }
+            return ret;
+        };
+        ll ans = INF;
+        dfs(arr, f, 0, ans);
+        if (ans==INF) ans = -1;
+        cout<<++tc<<". ";
+        cout<<ans<<endl;
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
+
