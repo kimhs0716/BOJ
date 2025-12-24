67983a44563f01ec9da73626c00d5af422b1154d
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-12-24 14:07:47 +0900

    [Silver III] Title: Karaoke, Time: 352 ms, Memory: 5884 KB -BaekjoonHub

---
+++ 백준/Silver/8795. Karaoke/Karaoke.cc
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O0,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    ll k; cin>>k;
+    string s; cin>>s;
+    ll n = s.size();
+    ll ans = 0;
+    ll cnt = 0;
+    set<char> vowels {'a', 'e', 'i', 'o', 'u', 'y'};
+    for (i=0;i<k;i++) {
+        if (vowels.contains(s[i])) cnt++;
+    }
+    if (cnt==k) ans++;
+    for (i=k;i<n;i++) {
+        if (vowels.contains(s[i])) cnt++;
+        if (vowels.contains(s[i-k])) cnt--;
+        if (cnt==k) ans++;
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
+
