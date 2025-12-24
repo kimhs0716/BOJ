794a12a3e557a092d9717d0794c4d1cec2ccf5d8
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-12-24 14:44:58 +0900

    [Gold III] Title: 0이 몇 개?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4276. 0이 몇 개？/0이 몇 개？.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    function<ll(ll)> f = [](ll n) -> ll {
+        if (n<0) return 0;
+        if (n==0) return 1;
+        ll ret = 1;
+        for (ll f=1; f<=n; f*=10) {
+            ll low = n%f;
+            ll cur = (n/f)%10;
+            ll high = n/(f*10);
+            if (high) {
+                ll add = (high-1)*f;
+                if (cur==0) add += low+1;
+                else add += f;
+                ret += add;
+            }
+        }
+        return ret;
+    };
+    while (true) {
+        ll n, m; cin>>n>>m;
+        if (n<0) break;
+        cout<<f(m) - f(n-1)<<endl;
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
+
