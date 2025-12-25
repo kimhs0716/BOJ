46990852f49b898dbceaf513c95d5bfe0f940d18
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-12-25 15:33:48 +0900

    [Silver III] Title: Password Security (Small1), Time: 120 ms, Memory: 110828 KB -BaekjoonHub

---
+++ 백준/Silver/12040. Password Security （Small1）/Password Security （Small1）.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda: sys.stdin.readline()[:-1]
+
+import random
+
+for tc in range(1, int(input())+1):
+    ans = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    n = int(input())
+    arr = input().split()
+    ok = [[1]*26 for _ in range(26)]
+    flag = True
+    for pw in arr:
+        if len(pw) == 1:
+            flag = False
+            break
+        if len(pw) == 2:
+            ok[ord(pw[0])-65][ord(pw[1])-65] = 0
+    for i in range(26):
+        if not any(ok[i]):
+            flag = False
+            break
+    print(f"Case #{tc}: ")
+    if not flag:
+        print("IMPOSSIBLE")
+        continue
+    while True:
+        random.shuffle(ans)
+        s = ''.join(ans)
+        if any(map(lambda x: x in s, arr)): continue
+        print(s)
+        break
