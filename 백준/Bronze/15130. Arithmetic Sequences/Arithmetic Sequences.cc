27dae2182e7201dd7652d7710a3cb0775d4c29a1
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2025-12-31 13:27:12 +0900

    [Bronze II] Title: Arithmetic Sequences, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15130. Arithmetic Sequences/Arithmetic Sequences.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    vl arr(10); cin>>arr;
+    ll l=0, r=0;
+    for (i=0;i<10;i++) if (arr[i]) r = i;
+    for (i=9;i>=0;i--) if (arr[i]) l = i;
+    if ((arr[r]-arr[l])%(r-l)) cout<<"-1\n";
+    else {
+        ll d = (arr[r]-arr[l])/(r-l);
+        for (i=l-1;i>=0;i--) arr[i] = arr[l]-d*(l-i);
+        for (i=l+1;i<10;i++) arr[i] = arr[l]+d*(i-l);
+        for (i=0;i<10;i++) cout<<arr[i]<<' '; cout<<endl;
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
