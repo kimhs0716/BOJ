daaae42fb66b4e26237b2507974de5d9f7364fc2
kimhsu0716 <82432843+kimhsu0716@users.noreply.github.com>
2026-01-12 22:45:13 +0900

    [Platinum IV] Title: Treasure Hunter, Time: 32 ms, Memory: 3724 KB -BaekjoonHub

---
+++ 백준/Platinum/23248. Treasure Hunter/Treasure Hunter.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    ll r, c, k; cin>>r>>c>>k;
+    vp arr(k);
+    for (auto &[x, y] : arr) cin>>x>>y;
+    sort(arr.begin(), arr.end());
+    vl ans;
+    for (auto [_, x] : arr) {
+        auto it = lower_bound(ans.begin(), ans.end(), -x);
+        if (it==ans.end()) ans.push_back(-x);
+        else *it = -x;
+    }
+    cout<<ans.size()<<endl;
+    // cout<<ans<<endl;
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
