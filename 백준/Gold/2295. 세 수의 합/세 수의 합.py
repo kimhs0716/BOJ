30de49902a0c635a541dcb5e62adefb5a6e7ac47
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-02-13 10:48:15 +0900

    [Gold IV] Title: 세 수의 합, Time: 252 ms, Memory: 178968 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,23 +1,15 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-try:
-    sys.stdin = open('boj.in', 'r', encoding='utf-8')
-except FileNotFoundError:
-    pass
-
 
 n = int(input())
 arr = [int(input()) for _ in range(n)]
-two = set()
-for i in arr:
-    for j in arr:
-        two.add(i + j)
 
+s = set(i+j for i in arr for j in arr)
 ans = 0
 for i in arr:
     for j in arr:
-        if i - j in two:
-            ans = max(ans, i)
-print(ans)
+        if i-j in s:
+            ans = max(i, ans)
 
+print(ans)
