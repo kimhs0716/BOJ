c904465437d730b295609e4d78d17c2683e4021d
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-02-15 12:09:47 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.cc
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    ll n, m; cin>>n>>m;
+    vp arr(n);
+    for (auto &x: arr) cin>>x.second;
+    for (auto &x: arr) cin>>x.first;
+    sort(arr.begin(), arr.end());
+    constexpr ll MAX = 10000;
+    vl dp(MAX+1);
+    for (auto [cost, mem] : arr) {
+        for (i=MAX-cost;i>=0;i--) {
+            dp[i+cost] = max(dp[i+cost], dp[i]+mem);
+        }
+    }
+    for (i=0;i<=MAX;i++) {
+        if (m<=dp[i]) {
+            cout<<i<<endl;
+            break;
+        }
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
