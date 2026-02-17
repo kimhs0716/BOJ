314dd9583c72a618504b4dc74721ec2e61cf726d
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-02-17 22:52:17 +0900

    [Gold IV] Title: Peculiar Primes, Time: 424 ms, Memory: 14768 KB -BaekjoonHub

---
+++ 백준/Gold/6638. Peculiar Primes/Peculiar Primes.cc
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc) {
+    ll i, j;
+    while (true) {
+        ll n; cin>>n;
+        if (!n) break;
+        vl arr(n); cin>>arr;
+        ll x, y; cin>>x>>y;
+        vl ans;
+        priority_queue<ll, vl, greater<>> pq;
+        set<ll> vis;
+        pq.push(1);
+        while (pq.size()) {
+            ll t = pq.top(); pq.pop();
+            if (x<=t) ans.push_back(t);
+            for (auto p: arr) {
+                if (t*p > y) continue;
+                if (vis.contains(t*p)) continue;
+                vis.insert(t*p);
+                pq.push(t*p);
+            }
+        }
+        if (ans.size()) {
+            for (i=0;i<ans.size();i++) {
+                cout<<ans[i]<<(i==ans.size()-1 ? '\n' : ',');
+            }
+        }
+        else {
+            cout<<"none\n";
+        }
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
