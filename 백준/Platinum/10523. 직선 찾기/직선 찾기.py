3cc3d0dc5377a09ea3708ebb07afde99d8118a5a
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-02-22 23:16:13 +0900

    [Platinum V] Title: 직선 찾기, Time: 308 ms, Memory: 121088 KB -BaekjoonHub

--- 백준/Platinum/10523. 직선 찾기/직선 찾기.py
+++ 백준/Platinum/10523. 직선 찾기/직선 찾기.py
@@ -12,26 +12,21 @@ if n==1:
 p = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-ITERS = 250
+ITERS = 75
 
 for _ in range(ITERS):
     i = random.randint(0, n-2)
     j = random.randint(i+1, n-1)
     a = arr[i]
     b = arr[j]
-    if a[0]==b[0]: # vertical
-        cnt = 0
-        for x, y in arr:
-            if x==a[0]: cnt += 1
-    else: # not vertical
-        cnt = 0
-        dx1 = a[0]-b[0]
-        dy1 = a[1]-b[1]
-        for x, y in arr:
-            dx2 = x-a[0]
-            dy2 = y-a[1]
-            if dx1*dy2 == dy1*dx2:
-                cnt += 1
+    cnt = 0
+    dx1 = a[0]-b[0]
+    dy1 = a[1]-b[1]
+    for x, y in arr:
+        dx2 = x-a[0]
+        dy2 = y-a[1]
+        if dx1*dy2 == dy1*dx2:
+            cnt += 1
     if 100*cnt >= p*n:
         print("possible")
         break
