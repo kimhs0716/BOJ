e6e681939ef94b149ac2ade583bb299f1a057b49
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-02-26 01:07:10 +0900

    [Silver IV] Title: 수면 장애, Time: 120 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/12755. 수면 장애/수면 장애.py
+++ 백준/Silver/12755. 수면 장애/수면 장애.py
@@ -4,10 +4,14 @@ input = lambda: sys.stdin.readline().rstrip()
 
 n = int(input())
 cur = 1
+l = 1
+nxt = 10
 while True:
-    l = len(str(cur))
     if n <= l:
         print(str(cur)[n-1])
         break
     n -= l
     cur += 1
+    if cur == nxt:
+        l += 1
+        nxt *= 10
