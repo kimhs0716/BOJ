98e4f81090c80468fc918b27dc344613a7b4077d
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-05 01:50:22 +0900

    [Gold III] Title: 자라나는 직교 나선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/10437. 자라나는 직교 나선/자라나는 직교 나선.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc){
+    ll i, j;
+    ll t, x, y; cin>>t>>x>>y;
+    cout<<t<<' ';
+    if (x<y) {
+        cout<<2<<' '<<x<<' '<<y<<endl;
+    }
+    else if (3<=x && 4<=y) {
+        ll b = x+2;
+        ll c = b+1;
+        ll a = c-y+2;
+        cout<<6<<' ';
+        cout<<"1 2 3 ";
+        cout<<a<<' '<<b<<' '<<c<<endl;
+    }
+    else cout<<"NO PATH\n";
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
