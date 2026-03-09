d19a2c3b3d5615182fda5c551ff3c77fb77400ff
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-09 13:45:46 +0900

    [Gold IV] Title: 팰린드롬?, Time: 256 ms, Memory: 33412 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc){
+    ll i, j;
+    ll n; cin>>n;
+    vl arr(n); cin>>arr;
+    vvl dp(n, vl(n));
+    for (i=0;i<n;i++) {
+        for (j=0;j<=i;j++) dp[i][j] = 1;
+    }
+    for (ll len=2;len<=n;len++) {
+        for (ll lo=0;lo+len-1<n;lo++) {
+            ll hi = lo+len-1;
+            if (arr[lo] == arr[hi]) dp[lo][hi] = dp[lo+1][hi-1];
+        }
+    }
+    // for (auto &x : dp) cout<<x<<endl;
+    ll q; cin>>q;
+    while (q--) {
+        ll a, b; cin>>a>>b;
+        cout<<dp[a-1][b-1]<<endl;
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
