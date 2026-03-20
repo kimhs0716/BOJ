f894146c289afcfbd4b2cdc5bd509040f242cec4
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-20 12:07:18 +0900

    [Gold II] Title: Kingdom’s Development Plan, Time: 84 ms, Memory: 10456 KB -BaekjoonHub

---
+++ 백준/Gold/32459. Kingdom’s Development Plan/Kingdom’s Development Plan.cc
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e15;
+constexpr ll MAX = 500'000;
+constexpr ld eps = 1e-8;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc){
+    ll i, j;
+    ll n, m; cin>>n>>m;
+    vl indeg(n);
+    vvl adj(n);
+    while (m--) {
+        ll u, v; cin>>u>>v;
+        --u; --v;
+        adj[u].push_back(v);
+        indeg[v]++;
+    }
+    priority_queue<ll, vl, greater<>> pq;
+    for (i=0;i<n;i++)
+        if (indeg[i]==0) {
+            pq.push(i);
+        }
+    vl ans;
+    while (pq.size()) {
+        ll cur = pq.top(); pq.pop();
+        ans.push_back(cur);
+        for (ll nxt : adj[cur]) {
+            indeg[nxt]--;
+            if (indeg[nxt]==0) pq.push(nxt);
+        }
+    }
+    if (ans.size()==n) {
+        for (auto x: ans) cout<<x+1<<' ';
+        cout<<endl;
+    }
+    else cout<<"IMPOSSIBLE\n";
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
