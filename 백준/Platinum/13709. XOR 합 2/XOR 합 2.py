c29ab1ff99c4adcd7137f6341255f6428d2d43ec
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-25 10:32:36 +0900

    [Platinum III] Title: XOR 합 2, Time: 1504 ms, Memory: 127324 KB -BaekjoonHub

---
+++ 백준/Platinum/13709. XOR 합 2/XOR 합 2.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+class GF2:
+    def __init__(self):
+        self.basis = set()
+
+    def add(self, x):
+        for b in self.basis:
+            for i in range(63, -1, -1):
+                if (b>>i)&1: break
+            if ((b>>i)&1) and ((x>>i)&1):
+                x ^= b
+        if x: self.basis.add(x)
+
+    def get(self):
+        ret = 0
+        for b in self.basis:
+            if (ret^b) > ret:
+                ret ^= b
+        return ret
+
+
+n = int(input())
+gf2 = GF2()
+for x in map(int, input().split()):
+    gf2.add(x)
+print(gf2.get())
