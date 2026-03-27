e2d2ca1b527738456a7d93ce4c32613eb0a55345
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-27 14:47:58 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 348 ms, Memory: 80168 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include <utility>
 #pragma GCC optimize ("O3,unroll-loops")
 #pragma GCC target ("avx,avx2,fma")
 
@@ -44,85 +46,221 @@ ostream& operator<<(ostream &os, vector<T> arr) {
 
 constexpr ll MOD = 1e9+7;
 // constexpr ll MOD = 998'244'353;
-constexpr ll INF = 1e15;
+constexpr ll INF = 1e18;
+constexpr ll MAX = 500'000;
+constexpr ld eps = 1e-8;
 
-struct LAZY {
-    ll n;
-    vl arr, seg, lazy;
-    LAZY(vl &a) : n(a.size()), arr(a), seg(n<<2), lazy(n<<2) {
-        init(1, 0, n-1);
-    }
-    void init(ll i, ll l, ll r) {
-        if (l==r) {
-            seg[i] = arr[l];
-            return;
-        }
-        ll m = l+r>>1;
-        init(i<<1, l, m);
-        init(i<<1|1, m+1, r);
-        seg[i] = seg[i<<1] + seg[i<<1|1];
-    }
-    void update_lazy(ll i, ll l, ll r) {
-        if (lazy[i]) {
-            seg[i] += (r-l+1) * lazy[i];
-            if (l!=r) {
-                lazy[i<<1] += lazy[i];
-                lazy[i<<1|1] += lazy[i];
-            }
-            lazy[i] = 0;
-        }
-    }
-    void update_range(ll i, ll l, ll r, ll s, ll e, ll val) {
-        update_lazy(i, l, r);
-        if (e<l || r<s) return;
-        if (s<=l && r<=e) {
-            seg[i] += (r-l+1) * val;
-            if (l!=r) {
-                lazy[i<<1] += val;
-                lazy[i<<1|1] += val;
-            }
-            return;
-        }
-        ll m=l+r>>1;
-        update_range(i<<1, l, m, s, e, val);
-        update_range(i<<1|1, m+1, r, s, e, val);
-        seg[i] = seg[i<<1] + seg[i<<1|1];
-    }
-    void update(ll s, ll e, ll val) {
-        update_range(1, 0, n-1, s, e, val);
-    }
-    ll query(ll i, ll l, ll r, ll s, ll e) {
-        update_lazy(i, l, r);
-        if (s<=l && r<=e) return seg[i];
-        if (e<l || r<s) return 0;
-        ll m=l+r>>1;
-        return query(i<<1, l, m, s, e) + query(i<<1|1, m+1, r, s, e);
-    }
-    ll query(ll s, ll e) {
-        return query(1, 0, n-1, s, e);
+void preprocess() {
+    ll i, j;
+}
+
+struct Node {
+    Node *p, *l, *r;
+    ll key, val, sum, sz, lazy;
+    Node(ll key, ll val) : key(key), val(val), sum(val) {
+        sz = 1;
+        lazy = 0;
+        p = l = r = nullptr;
     }
 };
 
-void preprocess() {
-    ll i, j, k;
+Node *root;
+
+void push(Node *x) {
+    if (!x || !x->lazy) return;
+    x->val += x->lazy;
+    if (x->l) {
+        x->l->lazy += x->lazy;
+        x->l->sum += x->lazy * x->l->sz;
+    }
+    if (x->r) {
+        x->r->lazy += x->lazy;
+        x->r->sum += x->lazy * x->r->sz;
+    }
+    x->lazy = 0;
 }
 
-void solve(ll testcase) {
-    ll i, j;
-    ll n, m, k; cin>>n>>m>>k;
-    vl arr(n); cin>>arr;
-    LAZY seg(arr);
-    m += k;
-    while (m--) {
-        ll a, b, c, d; cin>>a>>b>>c;
-        if (a==1) {
-            cin>>d;
-            seg.update(b-1, c-1, d);
+void update(Node *x) {
+    push(x);
+    x->sum = x->val;
+    x->sz = 1;
+    if (x->l) {
+        x->sum += x->l->sum;
+        x->sz += x->l->sz;
+    }
+    if (x->r) {
+        x->sum += x->r->sum;
+        x->sz += x->r->sz;
+    }
+}
+
+void rotate(Node *x) {
+    Node *p = x->p, *b = nullptr;
+    if (!p) return;
+    push(p);
+    push(x);
+    if (x == p->l) {
+        p->l = b = x->r;
+        x->r = p;
+    }
+    else {
+        p->r = b = x->l;
+        x->l = p;
+    }
+    x->p = p->p;
+    p->p = x;
+    if (b) b->p = p;
+    if (x->p) {
+        if (p == x->p->l) x->p->l = x;
+        else x->p->r = x;
+    }
+    else root = x;
+    update(p);
+    update(x);
+}
+
+void splay(Node *x, Node *g = nullptr) {
+    while (x->p != g) {
+        Node *p = x->p;
+        if (p->p == g) { // zig step
+            rotate(x);
+            break;
+        }
+        Node *pp = p->p;
+        if ((p->l==x) == (pp->l==p)) { // zig-zig step
+            rotate(p); rotate(x);
+        }
+        else { // zig-zag step
+            rotate(x); rotate(x);
+        }
+    }
+    if (!g) root = x;
+}
+
+void insert(ll key, ll val) {
+    Node *p = root;
+    if (!p) { // empty tree
+        Node *x = new Node(key, val);
+        root = x;
+        return;
+    }
+    while (true) {
+        if (key == p->key) return;
+        if (key < p->key) {
+            if (!p->l) break;
+            p = p->l;
         }
         else {
-            cout<<seg.query(b-1, c-1)<<endl;
+            if (!p->r) break;
+            p = p->r;
         }
-        // for (i=0;i<n;i++) cout<<seg.query(i, i)<<' '; cout<<endl;
+    }
+    Node *x = new Node(key, val);
+    x->p = p;
+    if (p->key > x->key) p->l = x;
+    else p->r = x;
+    splay(x);
+}
+
+bool find(ll key) { // find and splay
+    Node *p = root;
+    if (!p) return false;
+    while (p) {
+        if (key == p->key) break;
+        if (key < p->key) {
+            if (!p->l) break;
+            p = p->l;
+        }
+        else {
+            if (!p->r) break;
+            p = p->r;
+        }
+    }
+    splay(p);
+    return key == p->key;
+}
+
+void del(ll key) {
+    if (!find(key)) return;
+    Node *p = root;
+    if (p->l && p->r) { // root has two child nodes
+        root = p->l; // left child is new root node
+        root->p = nullptr;
+        // find maximum child of new root
+        Node *x = root;
+        while (x->r) x = x->r;
+        splay(x);
+        root->r = p->r;
+        p->r->p = root;
+        update(root);
+        delete p;
+        return;
+    }
+    if (p->l) { // root only has left child
+        root = p->l;
+        root->p = nullptr;
+        update(root);
+        delete p;
+        return;
+    }
+    if (p->r) { // root only has right child
+        root = p->r;
+        root->p = nullptr;
+        update(root);
+        delete p;
+        return;
+    }
+    // root has no child node
+    delete p;
+    root = nullptr;
+}
+
+void kth(ll k) { // splay kth node, 0-based
+    Node *x = root;
+    while (true) {
+        push(x);
+        while (x->l && x->l->sz > k) {
+            x = x->l;
+        }
+        if (x->l) k -= x->l->sz;
+        if (!k--) break;
+        x = x->r;
+    }
+    push(x);
+    splay(x);
+}
+
+Node *gather(ll s, ll e) {
+    kth(e+1);
+    auto temp = root;
+    kth(s-1);
+    splay(temp, root);
+    return root->r->l;
+}
+
+void solve(ll tc){
+    ll i, j, k;
+    ll n, q, qq; cin>>n>>q>>qq;
+    insert(0, 0);
+    insert(n+1, 0);
+    for (i=1;i<=n;i++) {
+        ll x; cin>>x;
+        insert(i, x);
+    }
+    q += qq;
+    while (q--) {
+        ll a, b, c, d; cin>>a>>b>>c;
+        if (a==1) { // add d to [b..c]
+            cin>>d;
+            auto x = gather(b, c);
+            x->lazy += d;
+            x->sum += d * x->sz;
+        }
+        else { // print sum of [b..c]
+            auto x = gather(b, c);
+            update(x);
+            cout<<x->sum<<endl;
+        }
     }
 }
 
@@ -136,4 +274,3 @@ int main() {
         solve(i);
     }
 }
-
