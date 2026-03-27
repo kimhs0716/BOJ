d601177a9aead427dcf0d6d76352b896f8b8b93e
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-03-27 18:54:19 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 360 ms, Memory: 80172 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -64,201 +64,208 @@ struct Node {
     }
 };
 
-Node *root;
+struct SplayTree {
+    Node *root;
 
-void push(Node *x) {
-    if (!x || !x->lazy) return;
-    x->val += x->lazy;
-    if (x->l) {
-        x->l->lazy += x->lazy;
-        x->l->sum += x->lazy * x->l->sz;
+    SplayTree() {
+        root = nullptr;
     }
-    if (x->r) {
-        x->r->lazy += x->lazy;
-        x->r->sum += x->lazy * x->r->sz;
-    }
-    x->lazy = 0;
-}
 
-void update(Node *x) {
-    push(x);
-    x->sum = x->val;
-    x->sz = 1;
-    if (x->l) {
-        x->sum += x->l->sum;
-        x->sz += x->l->sz;
-    }
-    if (x->r) {
-        x->sum += x->r->sum;
-        x->sz += x->r->sz;
-    }
-}
-
-void rotate(Node *x) {
-    Node *p = x->p, *b = nullptr;
-    if (!p) return;
-    push(p);
-    push(x);
-    if (x == p->l) {
-        p->l = b = x->r;
-        x->r = p;
-    }
-    else {
-        p->r = b = x->l;
-        x->l = p;
-    }
-    x->p = p->p;
-    p->p = x;
-    if (b) b->p = p;
-    if (x->p) {
-        if (p == x->p->l) x->p->l = x;
-        else x->p->r = x;
-    }
-    else root = x;
-    update(p);
-    update(x);
-}
-
-void splay(Node *x, Node *g = nullptr) {
-    while (x->p != g) {
-        Node *p = x->p;
-        if (p->p == g) { // zig step
-            rotate(x);
-            break;
+    void push(Node *x) {
+        if (!x || !x->lazy) return;
+        x->val += x->lazy;
+        if (x->l) {
+            x->l->lazy += x->lazy;
+            x->l->sum += x->lazy * x->l->sz;
         }
-        Node *pp = p->p;
-        if ((p->l==x) == (pp->l==p)) { // zig-zig step
-            rotate(p); rotate(x);
+        if (x->r) {
+            x->r->lazy += x->lazy;
+            x->r->sum += x->lazy * x->r->sz;
         }
-        else { // zig-zag step
-            rotate(x); rotate(x);
-        }
+        x->lazy = 0;
     }
-    if (!g) root = x;
-}
 
-void insert(ll key, ll val) {
-    Node *p = root;
-    if (!p) { // empty tree
-        Node *x = new Node(key, val);
-        root = x;
-        return;
-    }
-    while (true) {
-        if (key == p->key) return;
-        if (key < p->key) {
-            if (!p->l) break;
-            p = p->l;
-        }
-        else {
-            if (!p->r) break;
-            p = p->r;
-        }
-    }
-    Node *x = new Node(key, val);
-    x->p = p;
-    if (p->key > x->key) p->l = x;
-    else p->r = x;
-    splay(x);
-}
-
-bool find(ll key) { // find and splay
-    Node *p = root;
-    if (!p) return false;
-    while (p) {
-        if (key == p->key) break;
-        if (key < p->key) {
-            if (!p->l) break;
-            p = p->l;
-        }
-        else {
-            if (!p->r) break;
-            p = p->r;
-        }
-    }
-    splay(p);
-    return key == p->key;
-}
-
-void del(ll key) {
-    if (!find(key)) return;
-    Node *p = root;
-    if (p->l && p->r) { // root has two child nodes
-        root = p->l; // left child is new root node
-        root->p = nullptr;
-        // find maximum child of new root
-        Node *x = root;
-        while (x->r) x = x->r;
-        splay(x);
-        root->r = p->r;
-        p->r->p = root;
-        update(root);
-        delete p;
-        return;
-    }
-    if (p->l) { // root only has left child
-        root = p->l;
-        root->p = nullptr;
-        update(root);
-        delete p;
-        return;
-    }
-    if (p->r) { // root only has right child
-        root = p->r;
-        root->p = nullptr;
-        update(root);
-        delete p;
-        return;
-    }
-    // root has no child node
-    delete p;
-    root = nullptr;
-}
-
-void kth(ll k) { // splay kth node, 0-based
-    Node *x = root;
-    while (true) {
+    void update(Node *x) {
         push(x);
-        while (x->l && x->l->sz > k) {
-            x = x->l;
+        x->sum = x->val;
+        x->sz = 1;
+        if (x->l) {
+            x->sum += x->l->sum;
+            x->sz += x->l->sz;
         }
-        if (x->l) k -= x->l->sz;
-        if (!k--) break;
-        x = x->r;
+        if (x->r) {
+            x->sum += x->r->sum;
+            x->sz += x->r->sz;
+        }
     }
-    push(x);
-    splay(x);
-}
 
-Node *gather(ll s, ll e) {
-    kth(e+1);
-    auto temp = root;
-    kth(s-1);
-    splay(temp, root);
-    return root->r->l;
-}
+    void rotate(Node *x) {
+        Node *p = x->p, *b = nullptr;
+        if (!p) return;
+        push(p);
+        push(x);
+        if (x == p->l) {
+            p->l = b = x->r;
+            x->r = p;
+        }
+        else {
+            p->r = b = x->l;
+            x->l = p;
+        }
+        x->p = p->p;
+        p->p = x;
+        if (b) b->p = p;
+        if (x->p) {
+            if (p == x->p->l) x->p->l = x;
+            else x->p->r = x;
+        }
+        else root = x;
+        update(p);
+        update(x);
+    }
+
+    void splay(Node *x, Node *g = nullptr) {
+        while (x->p != g) {
+            Node *p = x->p;
+            if (p->p == g) { // zig step
+                rotate(x);
+                break;
+            }
+            Node *pp = p->p;
+            if ((p->l==x) == (pp->l==p)) { // zig-zig step
+                rotate(p); rotate(x);
+            }
+            else { // zig-zag step
+                rotate(x); rotate(x);
+            }
+        }
+        if (!g) root = x;
+    }
+
+    void insert(ll key, ll val) {
+        Node *p = root;
+        if (!p) { // empty tree
+            Node *x = new Node(key, val);
+            root = x;
+            return;
+        }
+        while (true) {
+            if (key == p->key) return;
+            if (key < p->key) {
+                if (!p->l) break;
+                p = p->l;
+            }
+            else {
+                if (!p->r) break;
+                p = p->r;
+            }
+        }
+        Node *x = new Node(key, val);
+        x->p = p;
+        if (p->key > x->key) p->l = x;
+        else p->r = x;
+        splay(x);
+    }
+
+    bool find(ll key) { // find and splay
+        Node *p = root;
+        if (!p) return false;
+        while (p) {
+            if (key == p->key) break;
+            if (key < p->key) {
+                if (!p->l) break;
+                p = p->l;
+            }
+            else {
+                if (!p->r) break;
+                p = p->r;
+            }
+        }
+        splay(p);
+        return key == p->key;
+    }
+
+    void del(ll key) {
+        if (!find(key)) return;
+        Node *p = root;
+        if (p->l && p->r) { // root has two child nodes
+            root = p->l; // left child is new root node
+            root->p = nullptr;
+            // find maximum child of new root
+            Node *x = root;
+            while (x->r) x = x->r;
+            splay(x);
+            root->r = p->r;
+            p->r->p = root;
+            update(root);
+            delete p;
+            return;
+        }
+        if (p->l) { // root only has left child
+            root = p->l;
+            root->p = nullptr;
+            update(root);
+            delete p;
+            return;
+        }
+        if (p->r) { // root only has right child
+            root = p->r;
+            root->p = nullptr;
+            update(root);
+            delete p;
+            return;
+        }
+        // root has no child node
+        delete p;
+        root = nullptr;
+    }
+
+    void kth(ll k) { // splay kth node, 0-based
+        Node *x = root;
+        while (true) {
+            push(x);
+            while (x->l && x->l->sz > k) {
+                x = x->l;
+            }
+            if (x->l) k -= x->l->sz;
+            if (!k--) break;
+            x = x->r;
+        }
+        push(x);
+        splay(x);
+    }
+
+    Node *gather(ll s, ll e) {
+        kth(e+1);
+        auto temp = root;
+        kth(s-1);
+        splay(temp, root);
+        return root->r->l;
+    }
+};
 
 void solve(ll tc){
     ll i, j, k;
     ll n, q, qq; cin>>n>>q>>qq;
-    insert(0, 0);
-    insert(n+1, 0);
+    SplayTree tree;
+    tree.insert(0, 0);
+    tree.insert(n+1, 0);
     for (i=1;i<=n;i++) {
         ll x; cin>>x;
-        insert(i, x);
+        tree.insert(i, x);
     }
     q += qq;
     while (q--) {
         ll a, b, c, d; cin>>a>>b>>c;
         if (a==1) { // add d to [b..c]
             cin>>d;
-            auto x = gather(b, c);
+            auto x = tree.gather(b, c);
             x->lazy += d;
             x->sum += d * x->sz;
         }
         else { // print sum of [b..c]
-            auto x = gather(b, c);
-            update(x);
+            auto x = tree.gather(b, c);
+            tree.update(x);
             cout<<x->sum<<endl;
         }
     }
