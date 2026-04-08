31bbeb9bc69cd27161bd41c190964c604a5175fd
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-04-08 16:25:07 +0900

    [Bronze IV] Title: Body Mass Index, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+// constexpr ll MOD = 1e9+7;
+constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e18;
+constexpr ld eps = 1e-8;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc){
+    ll i, j;
+    ll a; ld b;
+    cin>>a>>b;
+    ld bmi = a/(b*b);
+    if (bmi>25) cout<<"Overweight\n";
+    else if (bmi>=18.5) cout<<"Normal weight\n";
+    else cout<<"Underweight\n";
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
