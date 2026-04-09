d6d7a00d4b43a7ad891a5cfe74f3798a1eea0c54
김현수 <82432843+kimhs0716@users.noreply.github.com>
2026-04-10 00:21:05 +0900

    [Gold III] Title: 리그전 오브 레전드, Time: 72 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pll = pair<ll, ll>;
+using ld = long double;
+using vd = vector<ld>;
+using ull = unsigned long long;
+using vp = vector<pll>;
+using vvp = vector<vp>;
+using tlll = array<ll, 3>;
+
+#define endl '\n'
+
+void setup() {
+#ifdef KIMHS
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+template <typename T>
+istream& operator>>(istream &is, vector<T> &arr) {
+    for (auto &x: arr) is>>x;
+    return is;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, vector<T> arr) {
+    if (arr.size()==0) return os<<"()";
+    os<<'(';
+    for (ll i=0;i<arr.size()-1;i++) os<<arr[i]<<' ';
+    if (arr.size()) os<<arr.back();
+    return os<<')';
+}
+
+constexpr ll MOD = 1e9+7;
+// constexpr ll MOD = 998'244'353;
+constexpr ll INF = 1e18;
+constexpr ld eps = 1e-8;
+
+void preprocess() {
+    ll i, j;
+}
+
+void solve(ll tc){
+    ll i, j;
+    ll n, q; cin>>n>>q;
+    vl s1(n+1), s2(n+1);
+    for (i=1;i<=n;i++) {
+        ll x; cin>>x;
+        s1[i] = s1[i-1] + x;
+        s2[i] = s2[i-1] + x*x;
+    }
+    while (q--) {
+        ll l, r; cin>>l>>r;
+        ll x = s1[r] - s1[l-1];
+        ll y = s2[r] - s2[l-1];
+        cout<<(x*x-y)/2<<endl;
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+
+    ll testcase = 1;
+    // cin >> testcase;
+    for (ll i = 1; i <= testcase; i++) {
+        solve(i);
+    }
+}
